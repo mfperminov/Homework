325acc66e2d840df65c988b65df2b707d09e79a4
m.perminov <m.perminov@okko.tv>
2022-11-29 19:28:52 +0300

    third commit - add one more line

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Написал первую строчку");
         System.out.println("Написал вторую строчку");
+        System.out.println("Написал третьей строчку");
     }
 }
